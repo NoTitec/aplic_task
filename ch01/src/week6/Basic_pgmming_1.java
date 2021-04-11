7f2f62f73c77f9b9243e8ed02701f55ad68965bc
NoTitec <zkxkr0@gmail.com>
2021-04-11 22:03:25 +0900

    2021-4-11

--- ch01/src/week6/Basic_pgmming_1.java
+++ ch01/src/week6/Basic_pgmming_1.java
@@ -14,29 +14,51 @@ public class Basic_pgmming_1 {
 		case 1:
 			f.print10thnum();
 			break;
-		case 2:
+		case 3:
 			f.n10to2string();
 			break;
-		case 3:
-
+		case 5:
 			f.numjUnique(18, 3, 4);
 			f.numjUnique(6, 7, 6);
 			break;
-		case 4:
+		case 7:
 			int[] arr = { 74, 85, 102, 99, 101, 85, 56 };
 			f.lastindexOf(arr, 85);
 			break;
-		case 5:
+		case 9:
 			f.printwhatyear();
 			break;
-		case 6:
+		case 11:
 			int[] arr2 = { 14, 1, 22, 17, 36, 7, -43, 5 };
 			f.countInRange(arr2, 4, 17);
 			break;
-		case 7:
+		case 13:
 			int[] marr = { 27, 15, 15, 11, 27 };
 			f.mod(marr);
 			break;
+		case 15:
+			f.print_sqrt();
+			break;
+		case 17:
+			f.print_stars();
+			break;
+		case 19:
+			f.print_space_and_num();
+			break;
+		case 21:
+			f.print_bar_and_num();
+			break;
+		case 23:
+			f.print_nn();
+			break;
+		case 25:
+			int n=sc.nextInt();
+			f.printNumbers(n);
+			break;
+		case 27:
+			int s=sc.nextInt();
+			int h=sc.nextInt();
+			f.printSquare(s, h);
 		}
 		sc.close();
 	}
@@ -110,7 +132,7 @@ class methods6 {
 
 	public void mod(int[] a) {
 		int check[] = new int[a.length];// 빈도수저장배열생성
-		for (int i = 0; i < a.length; i++) {//빈도수0으로초기화
+		for (int i = 0; i < a.length; i++) {// 빈도수0으로초기화
 			check[i] = 0;
 
 		}
@@ -118,35 +140,109 @@ class methods6 {
 		for (int i = 0; i < a.length; i++) {
 
 			for (int j = i + 1; j < a.length; j++) {
-				if (a[i] == a[j]) {//같은수있으면그위치빈도수배열값증가
+				if (a[i] == a[j]) {// 같은수있으면그위치빈도수배열값증가
 					check[i]++;
 
-					if (check[maxidx] < check[i]) {//최빈값 위치찾기
+					if (check[maxidx] < check[i]) {// 최빈값 위치찾기
 						maxidx = i;
 					}
 				}
 			}
 		}
-		if (check[maxidx] == 0) {//초기인 0이면아무것도 없는것
+		if (check[maxidx] == 0) {// 초기인 0이면아무것도 없는것
 			System.out.println("없다.");
 			return;
 		}
-		int minval=0;
-		
-		for( int i=0;i<check.length;i++) {//
-			if(check[i]==check[maxidx]) {
-				minval=a[i];
+		int minval = 0;
+
+		for (int i = 0; i < check.length; i++) {//
+			if (check[i] == check[maxidx]) {
+				minval = a[i];
 			}
 		}
-		for(int i=check.length-1;i>=0;i--) {
-			if(check[i]==check[maxidx]) {
-				if(a[i]<minval) {
-					minval=a[i];
+		for (int i = check.length - 1; i >= 0; i--) {
+			if (check[i] == check[maxidx]) {
+				if (a[i] < minval) {
+					minval = a[i];
 				}
 			}
 		}
 		System.out.println(minval);
 		System.out.printf("\n");
+
+	}
+	public void print_sqrt() {
 		
+		for(int i=1;i<=10;i++)
+		{
+			int sqrval=0;
+			for(int j=0;j<i;j++) {
+				
+				sqrval=sqrval+i;
+			}
+			System.out.print(sqrval+" ");
+		}
+	}
+	public void print_stars() {
+		int i;
+		for(i=1;i<=5;i++) {
+			for(int j=1;j<i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int k=1;k<i-1;k++) {
+			System.out.print("*");
+		}
+	}
+	public void print_space_and_num() {
+		int spacecount;
+		int num=1;
+		for(spacecount=4;spacecount>=0;spacecount--) {
+			for(int j=spacecount;j>0;j--) {
+				System.out.print(" ");
+			}
+			System.out.println(num);
+			num++;
+		}
+	}
+	public void print_bar_and_num() {
+		for(int i=0;i<=5;i++) {
+			System.out.print("|       ");
+		}
+		System.out.println();
+		for(int j=0;j<=5;j++) {
+			for(int k=1;k<=9;k++) {
+				System.out.print(k);
+			}
+			System.out.print("0");
+		}
+	}
+	public void print_nn() {
+		for(int i=0;i<5;i++) {
+			for(int k=9;k>=0;k--) {
+				for(int j=0;j<5;j++) {
+					System.out.print(k);
+				}
+			}
+			System.out.println();
+		}
+	}
+	public void printNumbers(int n) {
+		for(int i=1;i<=n;i++) {
+			System.out.print("["+i+"]");
+		}
+	}
+	public void printSquare(int s, int h) {
+		 int i,j,k;
+		    for(i=s;i<=h;i++) {
+		        for(j=i;j<=h;j++) {
+		            System.out.print(j);
+		        }
+		        for(k=s;k<i;k++){
+		            System.out.print(k);
+		        }
+		        System.out.println();
+		    }
 	}
 }
