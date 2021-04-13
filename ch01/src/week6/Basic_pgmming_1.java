245912bc6b387720f11bb06f6aecb4790b9d1e1d
NoTitec <zkxkr0@gmail.com>
2021-04-13 20:41:16 +0900

    2021-4-13

--- ch01/src/week6/Basic_pgmming_1.java
+++ ch01/src/week6/Basic_pgmming_1.java
@@ -52,24 +52,36 @@ public class Basic_pgmming_1 {
 			f.print_nn();
 			break;
 		case 25:
-			int n=sc.nextInt();
+			int n = sc.nextInt();
 			f.printNumbers(n);
 			break;
 		case 27:
-			int s=sc.nextInt();
-			int h=sc.nextInt();
+			int s = sc.nextInt();
+			int h = sc.nextInt();
 			f.printSquare(s, h);
 			break;
 		case 29:
-			f.repeatTest("Java",3);
+			f.repeatTest("Java", 3);
 			break;
 		case 31:
 			int fuc;
-			fuc=sc.nextInt();
+			fuc = sc.nextInt();
 			f.showTwos(fuc);
 			break;
 		case 33:
 			f.diceSum();
+			break;
+		case 35:
+			f.endUp("Hello");
+			break;
+		case 37:
+			f.everyNth("Miracle", 2);
+			break;
+		case 39:
+			f.icyHot(120, -1);
+			f.icyHot(-1, 120);
+			f.icyHot(2, 120);
+			break;
 		}
 		sc.close();
 	}
@@ -182,110 +194,155 @@ class methods6 {
 		System.out.printf("\n");
 
 	}
+
 	public void print_sqrt() {
-		
-		for(int i=1;i<=10;i++)
-		{
-			int sqrval=0;
-			for(int j=0;j<i;j++) {
-				
-				sqrval=sqrval+i;
+
+		for (int i = 1; i <= 10; i++) {
+			int sqrval = 0;
+			for (int j = 0; j < i; j++) {
+
+				sqrval = sqrval + i;
 			}
-			System.out.print(sqrval+" ");
+			System.out.print(sqrval + " ");
 		}
 	}
+
 	public void print_stars() {
 		int i;
-		for(i=1;i<=5;i++) {
-			for(int j=1;j<i;j++) {
+		for (i = 1; i <= 5; i++) {
+			for (int j = 1; j < i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
-		for(int k=1;k<i-1;k++) {
+		for (int k = 1; k < i - 1; k++) {
 			System.out.print("*");
 		}
 	}
+
 	public void print_space_and_num() {
 		int spacecount;
-		int num=1;
-		for(spacecount=4;spacecount>=0;spacecount--) {
-			for(int j=spacecount;j>0;j--) {
+		int num = 1;
+		for (spacecount = 4; spacecount >= 0; spacecount--) {
+			for (int j = spacecount; j > 0; j--) {
 				System.out.print(" ");
 			}
 			System.out.println(num);
 			num++;
 		}
 	}
+
 	public void print_bar_and_num() {
-		for(int i=0;i<=5;i++) {
+		for (int i = 0; i <= 5; i++) {
 			System.out.print("|       ");
 		}
 		System.out.println();
-		for(int j=0;j<=5;j++) {
-			for(int k=1;k<=9;k++) {
+		for (int j = 0; j <= 5; j++) {
+			for (int k = 1; k <= 9; k++) {
 				System.out.print(k);
 			}
 			System.out.print("0");
 		}
 	}
+
 	public void print_nn() {
-		for(int i=0;i<5;i++) {
-			for(int k=9;k>=0;k--) {
-				for(int j=0;j<5;j++) {
+		for (int i = 0; i < 5; i++) {
+			for (int k = 9; k >= 0; k--) {
+				for (int j = 0; j < 5; j++) {
 					System.out.print(k);
 				}
 			}
 			System.out.println();
 		}
 	}
+
 	public void printNumbers(int n) {
-		for(int i=1;i<=n;i++) {
-			System.out.print("["+i+"]");
+		for (int i = 1; i <= n; i++) {
+			System.out.print("[" + i + "]");
 		}
 	}
+
 	public void printSquare(int s, int h) {
-		 int i,j,k;
-		    for(i=s;i<=h;i++) {
-		        for(j=i;j<=h;j++) {
-		            System.out.print(j);
-		        }
-		        for(k=s;k<i;k++){
-		            System.out.print(k);
-		        }
-		        System.out.println();
-		    }
+		int i, j, k;
+		for (i = s; i <= h; i++) {
+			for (j = i; j <= h; j++) {
+				System.out.print(j);
+			}
+			for (k = s; k < i; k++) {
+				System.out.print(k);
+			}
+			System.out.println();
+		}
 	}
-	
-	public void repeatTest(String s,int a) {
-		for(int i=0;i<a;i++) {
+
+	public void repeatTest(String s, int a) {
+		for (int i = 0; i < a; i++) {
 			System.out.print(s);
 		}
 	}
+
 	public void showTwos(int number) {
-		int count2=0;
-		int temp=number;
-		
-		while(temp%2==0) {
+		int count2 = 0;
+		int temp = number;
+
+		while (temp % 2 == 0) {
 			count2++;
-			temp=temp/2;
+			temp = temp / 2;
 		}
-		System.out.print(number+"=");
-		for(int i=0;i<count2;i++) {
+		System.out.print(number + "=");
+		for (int i = 0; i < count2; i++) {
 			System.out.print("2*");
 		}
 		System.out.println(temp);
 	}
+
 	public void diceSum() {
 		System.out.println("Desired dice sum: 9");
-		int sum=0;
+		int sum = 0;
 		int a;
 		int b;
-		while(sum!=9) {
-			a=sc.nextInt();
-			b=sc.nextInt();
-			sum=a+b;
-			System.out.println(a+"and"+b+"="+sum);
+		while (sum != 9) {
+			a = sc.nextInt();
+			b = sc.nextInt();
+			sum = a + b;
+			System.out.println(a + "and" + b + "=" + sum);
 		}
+	}
+
+	public void endUp(String str) {
+
+		String output = "";
+
+		int tmp;
+		System.out.print(str.substring(0, str.length() - 3));
+
+		for (int i = str.length() - 3; i < str.length(); i++) {
+			tmp = (int) str.charAt(i);
+			if ((97 <= tmp) && (tmp <= 122)) {
+				output += (char) (tmp - 32);
+			}
+		}
+		System.out.println(output);
+	}
+
+	public void everyNth(String str, int n) {
+
+		for (int i = 0; i < str.length(); i++) {
+			if (i % n == 0) {
+				System.out.print(str.charAt(i));
+			}
+		}
+
+	}
+	public void icyHot(int a,int b) {
+		boolean re=false;
+		if(a<0&&b>100) {
+			re=true;
+		}
+		else if(a>100&&b<0) {
+			re=true;
+		}
+		
+		System.out.println(re);
 	}
 }
